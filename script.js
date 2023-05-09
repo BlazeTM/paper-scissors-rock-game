a56b87dc45088359662f6b5a53b4ceba3923a7cd
Kacper Zapart <k4p1email@gmail.com>
2023-05-09 15:52:15 +0200

    Dodanie wyswietlania wynikow

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var computerWin=0;
 var draw = 0;
 var userChoice;
 var computerChoice;
+var resultss;
 const choices = ["Rock", "Scissors", "Paper", "Lizard", "Spock"];
 
 
@@ -16,11 +17,14 @@ function userRock(){
     if(userChoice == choices[0]){
         if(checkRock(computerChoice)){
             userWin = userWin+1;
+            resultss = "Wygrana"; 
         }else{
             if(computerChoice == choices[0]){
                 draw = draw+1;
+                resultss = "Remis"; 
             }else {
-                computerWin = computerWin+1;}
+                computerWin = computerWin+1;
+                resultss = "Przegrana"; }
         }
     }
     console.log(userWin,draw,computerWin);
@@ -34,11 +38,14 @@ function userScissors(){
     console.log(computerChoice);
         if(checkScissors(computerChoice)){
             userWin = userWin+1;
+            resultss = "Wygrana"; 
         }else{
             if(computerChoice == choices[1]){
                 draw = draw+1;
+                resultss = "Remis"; 
             }else {
-                computerWin = computerWin+1;}
+                computerWin = computerWin+1;
+                resultss = "Przegrana"; }
         }
     console.log(userWin,draw,computerWin);
     results();
@@ -52,11 +59,14 @@ function userPaper(){
     if(userChoice == choices[2]){
         if(checkPaper(computerChoice)){
             userWin = userWin+1;
+            resultss = "Wygrana"; 
         }else{
             if(computerChoice == choices[2]){
                 draw = draw+1;
+                resultss = "Remis"; 
             }else {
-                computerWin = computerWin+1;}
+                computerWin = computerWin+1;
+                resultss = "Przegrana"; }
         }
     }
     console.log(userWin,draw,computerWin);
@@ -70,11 +80,14 @@ function userLizard(){
     if(userChoice == choices[3]){
         if(checkLizard(computerChoice)){
             userWin = userWin+1;
+            resultss = "Wygrana"; 
         }else{
             if(computerChoice == choices[3]){
                 draw = draw+1;
+                resultss = "Remis"; 
             }else {
-                computerWin = computerWin+1;}
+                computerWin = computerWin+1;
+                resultss = "Przegrana"; }
         }
     }
     console.log(userWin,draw,computerWin);
@@ -88,11 +101,14 @@ function userSpock(){
     if(userChoice == choices[4]){
         if(checkSpock(computerChoice)){
             userWin = userWin+1;
+            resultss = "Wygrana"; 
         }else{
             if(computerChoice == choices[4]){
                 draw = draw+1;
+                resultss = "Remis"; 
             }else {
-                computerWin = computerWin+1;}
+                computerWin = computerWin+1;
+                resultss = "Przegrana"; }
         }
     }
     console.log(userWin,draw,computerWin);
@@ -150,6 +166,7 @@ function results(){
     document.getElementById("lose").innerHTML = computerWin;
     document.getElementById("userChoice").innerHTML = userChoice;
     document.getElementById("computerChoice").innerHTML = computerChoice;
+    document.getElementById("results").innerHTML = computerChoice;
 }
 
 function reset(){
